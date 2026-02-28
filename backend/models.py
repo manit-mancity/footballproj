5ac2e21b51f46075a29962a55902aea6f90177de
Manit Aravind <manitaravind2017@gmail.com>
2026-02-28 15:54:17 +0530

    second commit

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    matches_played = models.IntegerField()
+    wins = models.IntegerField()
+    draws = models.IntegerField()
+    losses = models.IntegerField()
+    goals_scored = models.IntegerField()
+    goals_conceded = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    goals = models.IntegerField()
+    assists = models.IntegerField()
+    matches_played = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Team, Player
+
+from django.shortcuts import render
+
+def dashboard(request):
+    teams = Team.objects.all()
+    players = Player.objects.all()
+
+    return render(request, 'dashboard.html', {
+        'teams': teams,
+        'players': players
+    })
